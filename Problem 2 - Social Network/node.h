7f6eb0c4cd48c590ae63ba02f95a5bec9848f877
AyushSahu-data <108819001+AyushSahu-data@users.noreply.github.com>
2022-11-17 23:58:15 +0530

    Add toCompare function

---
+++ Problem 2 - Social Network/node.h
@@ -0,0 +1,30 @@
+#ifndef DEF_NODE
+#define DEF_NODE
+#include "List.h"
+
+
+class Node {
+    private:
+        int id;
+        List * followingFriends;
+        List * followingAdversaries;
+
+    public:
+        Node(int id, List * followingFriends, List * followingAdversaries);
+        ~Node();
+        
+        // Getter
+        int getID();
+        List * getFollowingFriends();
+        int getNumberFollowingFriends();
+        List * getFollowingAdversaries();
+        int getNumberFollowingAdversaries();
+
+        // Other functions
+        bool hasFriends() ;
+        bool hasAdversaries();
+        void printNode();
+        bool toCompare(Node other);
+};
+
+#endif
