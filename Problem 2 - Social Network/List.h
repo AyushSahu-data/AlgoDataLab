b41e5b40108d508e7542b8dc3882230b1f11d032
AyushSahu-data <108819001+AyushSahu-data@users.noreply.github.com>
2022-11-17 23:55:11 +0530

    Implementation of social network algorithm not final version

---
+++ Problem 2 - Social Network/List.h
@@ -0,0 +1,52 @@
+#ifndef DEF_NODELIST
+#define DEF_NODELIST
+
+#include "Node.h"
+
+class Element {
+    private:
+        Node * data;
+        Element * nextElement;
+        Element * previousElement;
+    
+    public:
+        Element(Node * data, Element * nextElement, Element * previousElement);
+        ~Element();
+
+        // Getter
+        Node * getData();
+        Element * getNextElement();
+        Element * getPreviousElement();
+
+        // Setter
+        void setNextElement(Element * nextData);
+        void setPreviousElement(Element * previousElement);
+
+};
+
+// Définition de la classe Liste
+class List {
+    private:
+        Element * firstElement;
+        Element * lastElement;
+        int numberElement;
+
+    public:
+        List ();
+        ~List ();
+
+        // Getter
+        Element * getFirstElement ();
+        Element * getLastElement ();
+
+        // Other functions
+        void insertLast (Element * element);
+        void removeLast ();
+        void printList ();
+        int sizeList ();
+        
+};
+
+
+
+#endif
